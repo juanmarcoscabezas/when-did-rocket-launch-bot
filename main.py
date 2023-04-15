f5fa5bb2dac37e26b0ed045c60a9997d1c093265
Juan Marcos <juanmarcoscabezas@gmail.com>
2023-04-15 01:25:47 -0500

    text formated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request
 from pyngrok import conf, ngrok
-from waitress import serve
+from waitress import serve  # type: ignore
 
 from bot.handlers import bot, telebot
 
@@ -22,7 +22,8 @@ def index():
 @flask_server.route("/", methods=["POST"])
 def webhook():
     if request.headers.get("content-type") == "application/json":
-        update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
+        update = telebot.types.Update.de_json(
+            request.stream.read().decode("utf-8"))
         bot.process_new_updates([update])
         return "OK", 200
 
@@ -38,17 +39,17 @@ if __name__ == "__main__":
 
     if BOT_POLLING == "True":
         bot.infinity_polling()
-
-    conf.get_default().config_path = "./config_ngrok.yml"
-    conf.get_default().region = "eu"
-    ngrok.set_auth_token(NGROK_TOKEN)
-
-    ngrok_tunel = ngrok.connect(SERVER_PORT, bind_tls=True)
-    ngrok_url = ngrok_tunel.public_url
-    print("URL NGROK:", ngrok_url)
-
-    bot.set_webhook(url=ngrok_url)
-    if ENV == "prod":
-        serve(flask_server, host="0.0.0.0", port=SERVER_PORT)
     else:
-        flask_server.run(host="0.0.0.0", port=SERVER_PORT)
+        conf.get_default().config_path = "./config_ngrok.yml"
+        conf.get_default().region = "eu"
+        ngrok.set_auth_token(NGROK_TOKEN)
+
+        ngrok_tunel = ngrok.connect(SERVER_PORT, bind_tls=True)
+        ngrok_url = ngrok_tunel.public_url
+        print("URL NGROK:", ngrok_url)
+
+        bot.set_webhook(url=ngrok_url)
+        if ENV == "prod":
+            serve(flask_server, host="0.0.0.0", port=SERVER_PORT)
+        else:
+            flask_server.run(host="0.0.0.0", port=SERVER_PORT)
--- utils/texts.py
+++ utils/texts.py
@@ -1,8 +1,9 @@
 CAPTION = 'Did the rocket launch?'
-MESSAGE_NOT_MODIFIED = '''
-    message is not modified:
-    specified new message content and reply markup are exactly the same
-    '''
+MESSAGE_NOT_MODIFIED = '{} {}'\
+    .format(
+        'message is not modified: specified new message',
+        'content and reply markup are exactly the same'
+    )
 CONGRATULATIONS = '''Congratulations,
 you have found the frame in which the rocket launches,
 The frame is number "{}"'''
